ab41901a65f3dfae2426aa8c2319315301dd0401
DavigiraldiMichels <davigmichels@gmail.com>
2024-02-06 17:59:58 -0300

    aula de layout

---
+++ resources/views/blog/artigos.blade.php
@@ -0,0 +1,7 @@
+@extends("layout.blog")
+@section("title", "Artigos do blog")
+@section("content")
+
+<h1>Aula de programação</h1>
+
+@endsection
--- resources/views/blog/home.blade.php
+++ resources/views/blog/home.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <title>Document</title>
-</head>
-<body>
-    <h1 class="text-info">Seja bem vindos</h1>
-    <button class="btn btn-chief">BOTÃO DO DANIEL PERNA AQUI</button>
-    <script src="/js/bootstrap.min.js"></script>
-    <script src="/js/app.js"></script>
-</body>
-</html>
+@extends("layout.blog") {{--layout/blog--}}
+@section("title", "Blog do café pra dev") {{-- determina a injeção de um conteúdo --}}
+@section("content")
+
+<h1>blog do café pra dev</h1>
+
+@endsection
