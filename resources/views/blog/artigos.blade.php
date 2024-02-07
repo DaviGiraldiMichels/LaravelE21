4ac2d568f025851067287aca693eec88c5814228
DavigiraldiMichels <davigmichels@gmail.com>
2024-02-07 18:39:28 -0300

    aula de sass

--- resources/views/blog/artigos.blade.php
+++ resources/views/blog/artigos.blade.php
@@ -3,5 +3,8 @@
 @section("content")
 
 <h1>Aula de programação</h1>
-
+<button class="btn btn-verdeCaganeira">Aula de programação</button>
+<div class="alert bg-green" role="alert">
+    A simple primary alert—check it out!
+</div>
 @endsection
--- resources/views/layout/blog.blade.php
+++ resources/views/layout/blog.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     {{-- @yield exibe o conteúdo de uma determinada sessão --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>@yield("title")</title>
 </head>
 <body>
@@ -18,11 +18,7 @@
         </div>
     </main>
 
-    <footer>
+    @include("layout.footer")
 
-
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
